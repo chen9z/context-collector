f83b295a1d82ca74bcdd9c8fb3ca1009e029f92a
chen9z <yclooper@gmail.com>
2024-08-13 21:21:27 +0800

    update code

--- src/context_collector/app.py
+++ src/context_collector/app.py
@@ -4,9 +4,58 @@ import os.path
 
 from pathlib import Path
 
-from context_collector.default_ignores import DEFAULT_IGNORES
+DEFAULT_IGNORES = [
+    # Version control and IDE related
+    '.git/',
+    '.gitignore',
+    '.idea/',
+    '__pycache__',
+    '*.pyc',
+    '*.pyo',
+    '*.pyd',
+    '.DS_Store',
 
+    # java jar
+    '*.jar',
 
+    # Image formats
+    '*.jpg',
+    '*.jpeg',
+    '*.png',
+    '*.gif',
+    '*.bmp',
+    '*.svg',
+    '*.ico',
+
+    # Documents and presentations
+    '*.pdf',
+    '*.doc',
+    '*.docx',
+    '*.ppt',
+    '*.pptx',
+    '*.xls',
+    '*.xlsx',
+
+    # Compressed files
+    '*.zip',
+    '*.rar',
+    '*.7z',
+    '*.tar',
+    '*.gz',
+
+    # Audio and video files
+    '*.mp3',
+    '*.wav',
+    '*.mp4',
+    '*.avi',
+    '*.mov',
+
+    # Other binary files
+    '*.exe',
+    '*.dll',
+    '*.so',
+    '*.dylib'
+]
 def get_ignore_patterns(path: str):
     gitignore_path = os.path.join(path, ".gitignore")
     if os.path.exists(gitignore_path):
